d09348e84af1a5a0c6b30a64e7298d64e6239616
Taras Zhurenko <zhta21@yandex.ru>
2021-12-07 23:50:28 +0300

    Create Bonus

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
 public class Main {
     public static void main(String[] args) {
 
+        int currentAmount = 100;
+        int depositAmount = 1_999;
+        int oneBonus = 100;
+
+        int bonusAmount;
+        if (depositAmount > 1000) {
+            bonusAmount = depositAmount / oneBonus;
+        } else {
+            bonusAmount = 0;
+        }
+
+        int totalAmount = currentAmount + depositAmount + bonusAmount;
+        System.out.println(totalAmount + " (Включая бонусных рублей: " + bonusAmount + ")");
     }
 }
